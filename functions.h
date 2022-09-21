a63dc85bcf14dc47499554355ce45a120d9836fb
justinw21 <justinw21@DESKTOP-1K8PKNG.localdomain>
2022-09-21 13:26:28 -0400

    commit

--- functions.h
+++ functions.h
@@ -1,2 +1,8 @@
 #pragma once
-int box(int width, int height);
+std::string box(int width, int height);
+std::string checkerBoard(int width, int height);
+std::string cross(int size);
+std::string lower(int size);
+std::string upper(int size);
+std::string trapezoid(int width, int height);
+std::string checkerboard3x3(int width, int height);
