d073b62cea658876b8e5983d05d338f34876dbf9
justinw21 <justinw21@DESKTOP-1K8PKNG.localdomain>
2022-09-21 13:26:47 -0400

    commit

--- main.cpp
+++ main.cpp
@@ -3,5 +3,19 @@
 #include <string.h>
 
 int main(){
+    std::string increment = "\n-------------------------------------------- ";
     box(5,4);
+    std::cout << increment << std::endl;
+    checkerBoard(11,6);
+    std::cout << increment << std::endl;
+    cross(8);
+    std::cout << increment << std::endl;
+    lower(6);
+    std::cout << increment << std::endl;
+    upper(6);
+    std::cout << increment << std::endl;
+    trapezoid(12, 5);
+    std::cout << increment << std::endl;
+    checkerboard3x3(27,27);
+
 }
